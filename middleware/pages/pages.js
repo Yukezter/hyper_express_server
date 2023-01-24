656e0455e4ba1ba1c4cfbc8ef3eb03755c0f9482
Bryan Hinchliffe <31435212+Yukezter@users.noreply.github.com>
2023-01-24 09:29:46 -0800

    Update pages.js

--- middleware/pages/pages.js
+++ middleware/pages/pages.js
@@ -1,16 +1,10 @@
 import render from '../../utils/render.js'
 
-const isAuthenticated = false
-
 // Attatches a 'render' function to the response object
 // The function sends a response back with ejs rendered html
 const pages_middleware = (req, res, next) => {
   console.log('pages middleware')
-
-  if (isAuthenticated) {
-    return res.redirect('/')
-  }
-
+  
   res.render = async (path = req.path) => {
     try {
       const html = await render(path)
