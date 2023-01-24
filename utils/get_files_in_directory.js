ba8e0becf6beff8e381f413f96808e10eb6b4eec
Bryan Hinchliffe <31435212+Yukezter@users.noreply.github.com>
2023-01-24 09:32:46 -0800

    Update and rename getFilesInDirectory.js to get_files_in_directory.js

--- utils/get_files_in_directory.js
+++ utils/get_files_in_directory.js
@@ -1,22 +1,22 @@
 import fs from 'fs'
 import path from 'path'
 
-const getFilesInDirectory = directory => {
+const get_files_in_directory = directory => {
   const files = []
-  const getFilesRecursively = dir => {
+  const get_files_recursively = dir => {
     const filesInDirectory = fs.readdirSync(dir)
     for (const file of filesInDirectory) {
       const absolute = path.join(dir, file)
       if (fs.statSync(absolute).isDirectory()) {
-        getFilesRecursively(absolute)
+        get_files_recursively(absolute)
       } else {
         files.push(absolute)
       }
     }
   }
 
-  getFilesRecursively(directory)
+  get_files_recursively(directory)
   return files.map(file => file.replace(new RegExp('\\\\', 'g'), '/'))
 }
 
-export default getFilesInDirectory
+export default get_files_in_directory
