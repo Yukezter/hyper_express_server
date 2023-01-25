7769d2fa3e9fecbda5a885c33593490b1856392a
Bryan Hinchliffe <31435212+Yukezter@users.noreply.github.com>
2023-01-24 23:28:07 -0800

    Update auth.js

--- middleware/api/auth.js
+++ middleware/api/auth.js
@@ -3,7 +3,7 @@ const is_authenticated = false
 const auth_middleware = async (req, res) => {
   console.log('auth middleware')
   if (!is_authenticated) {
-    res.status(403).send('Forbidden')
+    res.status(403).end('Forbidden')
   }
 }
 
